20d7c5e79ca5034d56c6855e8a42a2fea1ad8d23
Gracious Nguwo <graciousnguwo2020@gmail.com>
2021-09-08 20:43:05 +0200

    hello, write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,5 @@
 import sys
 #!/usr/bin/python3
 import sys
-sys.stderr.write(str('and that piece of art is useful\
- - Dora Korpar, 2015-10-19') + '\n')
+sys.stderr.write(str('and that piece of art is useful - Dora Korpar, 2015-10-19') + '\n')
 exit(1)
